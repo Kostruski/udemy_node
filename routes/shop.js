f088d2bcc45c8c28992ef2f69788b58923816eb5
Marek-Kostruski <mkkos123@gmail.com>
2020-01-14 13:31:48 +0100

    add pug

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,13 @@
 const path = require('path');
-
 const express = require('express');
-
 const rootDir = require('../util/path');
+const adminData = require('../routes/admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log(adminData.products);
 });
 
 module.exports = router;
