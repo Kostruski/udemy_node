ee04eb4be17e167c440db01e6d4ea321bd302b06
Marek-Kostruski <mkkos123@gmail.com>
2020-01-14 16:51:06 +0100

    finished handlebars

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const expressHbs = require('express-handlebars');
 const app = express();
 
 app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname: 'hbs',
+    }),
 );
 app.set('view engine', 'hbs');
 app.set('views', 'views');
@@ -27,7 +27,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
