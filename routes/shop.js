1fcdf2601c9435a37a299664fdb274e30fb753a8
Marek-Kostruski <mkkos123@gmail.com>
2020-01-14 14:29:59 +0100

    finish_pug

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,15 @@
 const path = require('path');
+
 const express = require('express');
+
 const rootDir = require('../util/path');
-const adminData = require('../routes/admin');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    console.log(adminData.products);
+  const products = adminData.products;
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
