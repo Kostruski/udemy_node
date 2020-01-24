5c9bd20038605b318542fae96c05dc4d54e59f65
Marek-Kostruski <mkkos123@gmail.com>
2020-01-24 11:29:01 +0100

    uplode from udemy

--- models/product.js
+++ models/product.js
@@ -1,42 +1,37 @@
 const fs = require('fs');
 const path = require('path');
-const utils = require('../utils/utils');
 
-const { mainFolder } = utils;
-const p = path.join(mainFolder, 'data', 'products.js');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const fileContent = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(p, (err, data) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        });
-    });
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
 };
 
 module.exports = class Product {
-    constructor(title) {
-        this.title = title;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    async save() {
-        let products = [];
-        try {
-            const productsFromFile = await fileContent();
-            const productsParsed = await JSON.parse(productsFromFile);
-            products = productsParsed;
-        } catch (error) {
-            console.log('error in reading products from file', error);
-        }
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), error => {
-            console.log(error);
-        });
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return fileContent();
-    }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 };
