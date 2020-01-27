8e238e2e85f200f636c372fbf9afcf1d79a627ed
Marek-Kostruski <mkkos123@gmail.com>
2020-01-27 13:53:48 +0100

    add deleteProduct

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,31 +1,37 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
+    res.render('admin/add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true,
+    });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+    const { title } = req.body;
+    const { imageUrl } = req.body;
+    const { price } = req.body;
+    const { description } = req.body;
+    const product = new Product(title, imageUrl, description, price);
+    product.save();
+    res.redirect('/admin/products');
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+    Product.fetchAll(products => {
+        res.render('admin/products', {
+            prods: products,
+            pageTitle: 'Admin Products',
+            path: '/admin/products',
+        });
     });
-  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
 };
