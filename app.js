2319d33a1ee4b90538c4cdab25ae72ee3a27054a
Marek-Kostruski <mkkos123@gmail.com>
2020-02-16 18:36:29 +0100

    set new password

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDbStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+    'mongodb+srv://Marek:12345@udemy-j7mmt.mongodb.net/shop?authSource=admin&replicaSet=udemy-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+
 const app = express();
+
+const store = new MongoDbStore({ uri: MONGODB_URI, collection: 'sessions' });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -18,27 +25,31 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    session({
+        secret: 'my secret',
+        resave: false,
+        saveUninitialized: false,
+        store,
+    }),
+);
 
-app.use((req, res, next) => {
-    User.findById('5bab316ce0a7c75f783cb8a8')
-        .then(user => {
-            req.user = user;
-            next();
-        })
-        .catch(err => console.log(err));
-});
-
+// app.use((req, res, next) => {
+//     User.findById('5bab316ce0a7c75f783cb8a8')
+//         .then(user => {
+//             req.session.user = user;
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
+app.use(authRoutes);
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-    .connect(
-        'mongodb+srv://Marek:12345@udemy-j7mmt.mongodb.net/test?authSource=admin&replicaSet=udemy-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
-        { useNewUrlParser: true, useUnifiedTopology: true },
-    )
+    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => {
         User.findOne().then(user => {
             if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
-  });
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: req.session.isLoggedIn,
+    });
 };
 
-exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+exports.postLogin = async (req, res, next) => {
+    const user = await User.findById('5bab316ce0a7c75f783cb8a8');
+    req.session.user = user;
+    req.session.isLoggedIn = true;
     res.redirect('/');
-  };
+};
